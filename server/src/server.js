9c3648718a0691cd8c98f528f2e64dcc17752719
idrees <idreesibraheem@gmail.com>
2017-11-18 14:30:25 +0100

    Set up package.json, babel, eslint and express server

--- server/src/server.js
+++ server/src/server.js
@@ -1,7 +1,9 @@
 /* import modules */
+import 'babel-polyfill';
 import express from 'express';
 import bodyParser from 'body-parser';
 import morgan from 'morgan';
+
 
 /* initialise App */
 
@@ -9,9 +11,9 @@ const app = express();
 const port = process.env.PORT || 5050;
 
 app.get('/home/', (req, res) => {
-  res.status(200).send({ message: 'Locked and Loaded'});
+  res.status(200).send({ message: 'Locked and Loaded' });
 });
 
 app.listen(port, () => {
-  console.log(`we are live on port ${port}`);
+//  console.log(`we are live on port ${port}`);
 });
