79e0a8c9023d1729fa970d4bf7861876a450c13f
idrees <idreesibraheem@gmail.com>
2017-11-18 11:27:42 +0100

    Setup server-side project environment and install packages

---
+++ server/src/server.js
@@ -0,0 +1,17 @@
+/* import modules */
+import express from 'express';
+import bodyParser from 'body-parser';
+import morgan from 'morgan';
+
+/* initialise App */
+
+const app = express();
+const port = process.env.PORT || 5050;
+
+app.get('/home/', (req, res) => {
+  res.status(200).send({ message: 'Locked and Loaded'});
+});
+
+app.listen(port, () => {
+  console.log(`we are live on port ${port}`);
+});
