f49908eb5b6a09170f0d0da9f4cf13cdd9cacf8c
idrees <idreesibraheem@gmail.com>
2017-11-21 00:10:27 +0100

    Add endpoint '/api/v1/centers/' for creating new centers

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 
 [![Build Status](https://travis-ci.org/iidrees/Events-Manager.svg?branch=develop)](https://travis-ci.org/iidrees/Events-Manager)
-[![Coverage Status](https://coveralls.io/repos/github/iidrees/Events-Manager/badge.svg?branch=ch-setup-test-152986115)](https://coveralls.io/github/iidrees/Events-Manager?branch=ch-setup-test-152986115)
+[![Coverage Status](https://coveralls.io/repos/github/iidrees/Events-Manager/badge.svg)](https://coveralls.io/github/iidrees/Events-Manager)
 [![Maintainability](https://api.codeclimate.com/v1/badges/507b075d1aa0f1e22e24/maintainability)](https://codeclimate.com/github/iidrees/Events-Manager/maintainability)
 [![Test Coverage](https://api.codeclimate.com/v1/badges/507b075d1aa0f1e22e24/test_coverage)](https://codeclimate.com/github/iidrees/Events-Manager/test_coverage)
 
--- server/src/models/center.js
+++ server/src/models/center.js
@@ -2,27 +2,67 @@ export default (sequelize, DataTypes) => {
   const Centers = sequelize.define('Centers', {
     name: {
       type: DataTypes.STRING,
-      allowNull: false
+      allowNull: false,
+      validate: {
+        notEmpty: {
+          args: true,
+          msg: 'Please enter a CENTER NAME in the input field'
+        }
+      },
+      unique: {
+        args: true,
+        msg: 'Please enter another Center Name'
+      }
     },
     location: {
       type: DataTypes.STRING,
-      allowNull: false
+      allowNull: false,
+      validate: {
+        notEmpty: {
+          args: true,
+          msg: 'Please enter a Location i.e a state where the center is located'
+        }
+      }
     },
     address: {
       type: DataTypes.STRING,
-      allowNull: false
+      allowNull: false,
+      validate: {
+        notEmpty: {
+          args: true,
+          msg: 'Please enter a clear address'
+        }
+      }
     },
     owner: {
       type: DataTypes.STRING,
-      allowNull: false
+      allowNull: false,
+      validate: {
+        notEmpty: {
+          args: true,
+          msg: 'Please enter enter the name of the owner'
+        }
+      }
     },
     capacity: {
       type: DataTypes.STRING,
-      allowNull: false
+      allowNull: false,
+      validate: {
+        notEmpty: {
+          args: true,
+          msg: 'Please enter the capacity of the Center'
+        }
+      }
     },
     description: {
       type: DataTypes.STRING,
-      allowNull: false
+      allowNull: false,
+      validate: {
+        notEmpty: {
+          args: true,
+          msg: 'Please enter a description of what the center looks like'
+        }
+      }
     }
   });
 
